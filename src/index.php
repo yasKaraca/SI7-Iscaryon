98b37c856de4dcc4d2c7bd4a34a70fc562cb5abb
joel2035 <joel.pokam@hetic.net>
2018-07-31 10:21:46 +0200

    modif

--- src/index.php
+++ src/index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="styles/">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+
+
 </head>
 <body>
 
---
+++ src/way.php
@@ -0,0 +1,86 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
+</head>
+<body>
+<section class="way">
+  <div class="container">
+  <div class="row">
+  <div class="way-content">
+    <div class="way-contentOne one">
+      <p>1</p>
+    </div>
+    <div class="way-bar">
+
+    </div>
+    <div class="way-contentOne">
+      <p>2</p>
+    </div>
+    <div class="way-bar">
+
+    </div>
+    <div class="way-contentOne">
+      <p>3</p>
+    </div>
+    <div class="way-bar">
+
+    </div>
+    <div class="way-contentOne">
+      <p>4</p>
+    </div>
+  </div>
+  </div>
+  </div>
+</section>
+<section class="choice">
+    <div class="container">
+      <div class="row choice-content">
+
+        <div class="col-xs-12">
+          <h1 class="choice-title">
+          Quel types de produits vendez-vous?
+        </h1>
+        </div>
+        <div class="choice-box col-xs-12 col-md-4">
+          <div class="choice-boxContent">
+            <h2 class="choice-boxContentTitle">
+               Alimentaires
+             </h2>
+
+          </div>
+        </div>
+        <div class="choice-box  col-xs-12 col-md-4">
+          <div class="choice-boxContent">
+            <h2 class="choice-boxContentTitle">
+            Technologie
+             </h2>
+
+          </div>
+        </div>
+        <div class="choice-box  col-xs-12 col-md-4">
+          <div class="choice-boxContent">
+            <h2 class="choice-boxContentTitle">
+               Pierre precieuse
+             </h2>
+
+          </div>
+        </div>
+        <div class="choice-box  col-xs-12 col-md-4">
+          <dv class="choice-boxContent">
+            <h2 class="choice-boxContentTitle">
+              Vestimentaire
+             </h2>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</body>
+</html>
